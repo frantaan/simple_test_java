ec78e65062e20a60965fe607caccc34879f7d6a7
raaparin <roman.aparin@migogroup.ru>
2020-10-07 13:55:49 +0400

    first commit

--- Docker/run_tests.sh
+++ Docker/run_tests.sh
@@ -5,5 +5,6 @@ docker pull selenoid/vnc:firefox_47.0
 docker pull selenoid/video-recorder:latest-release
 echo " starting Selenoid Service ....... "
 docker-compose up --force-recreate -d
+cd ..
 mvn test -Pweb_execution -Dsuite=dataprovider -Dtestng.dtd.http=true -Dio.netty.tryReflectionSetAccessible=true -Dcom.couchbase.client.core.deps.io.netty.noUnsafe=true
-al
+allure serve
